f54934521332a79aee92fe2ad58ccfadf3921d09
saki <16546851+sakiko-2@users.noreply.github.com>
2020-08-23 19:34:58 +1200

    Basic layout

---
+++ src/screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,110 @@
+import React, { useState } from 'react';
+import { Image, Text, TextInput, TouchableOpacity, StyleSheet, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+export default function LoginScreen({navigation}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onFooterPress = () => {
+    navigation.navigate('Registration')
+  };
+
+  const onLoginPress = () => {
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        keyboardShouldPersistTaps="always"
+      >
+        <Image
+          source={require('../../../assets/icon.png')}
+          style={styles.logo}
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaa"
+          placeholder="Email"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaa"
+          secureTextEntry
+          placeholder="Password"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onLoginPress()}
+        >
+          <Text style={styles.buttonTitle}>Log in</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>Don't have an account?</Text>
+          <Text onPress={onFooterPress} style={styles.footerLink}>Sign up</Text>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 30,
+    backgroundColor: 'lightgrey'
+  },
+  title: {
+
+  },
+  logo: {
+    height: 120,
+    width: 120,
+    marginBottom: 30,
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+  input: {
+    height: 48,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1cb0f6',
+    borderRadius: 10,
+    marginVertical: 10,
+    height: 48
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  footerView: {
+    alignItems: 'center',
+    marginTop: 20
+  },
+  footerText: {
+    fontSize: 16,
+  },
+  footerLink: {
+    color: '#1cb0f6',
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 10
+  }
+});
---
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -0,0 +1,130 @@
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+export default function RegistrationScreen({navigation}) {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmWPassword] = useState('');
+
+  const onFooterPress = () => {
+    navigation.navigate('Login');
+  };
+
+  const onRegisterPress = () => {
+
+  };
+
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView>
+        <Image
+          style={styles.logo}
+          source={require('../../../assets/icon.png')}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          placeholderTextColor="#aaa"
+          onChangeText={(text) => setFullName(text)}
+          value={fullName}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#aaa"
+          onChangeText={(text) => setEmail(text)}
+          value={email}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          placeholderTextColor="#aaa"
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirm Password"
+          secureTextEntry
+          placeholderTextColor="#aaa"
+          onChangeText={(text) => setConfirmPassword(text)}
+          value={confirmPassword}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => onRegisterPress()}
+        >
+          <Text style={styles.buttonTitle}>Create account</Text>
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.footerText}>Already have an account?
+            <Text onPress={onFooterPress} style={styles.footerLink}> Log in</Text>
+          </Text>
+        </View>
+      </KeyboardAwareScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'lightgrey',
+    padding: 30
+  },
+  title: {
+
+  },
+  logo: {
+    height: 120,
+    width: 120,
+    marginBottom: 30,
+    marginTop: 10,
+    alignSelf: 'center',
+  },
+  input: {
+    height: 48,
+    borderRadius: 10,
+    overflow: 'hidden',
+    backgroundColor: 'white',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#1cb0f6',
+    borderRadius: 10,
+    marginVertical: 10,
+    height: 48
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  footerView: {
+    alignItems: 'center',
+    marginTop: 20
+  },
+  footerText: {
+    fontSize: 16,
+  },
+  footerLink: {
+    color: '#1cb0f6',
+    fontSize: 16,
+    fontWeight: '500',
+    marginTop: 10
+  }
+});
